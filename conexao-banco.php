331774f208991c5c837eaae0d00cfc642811128e
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2022-12-01 15:15:49 -0300

    Update conexao-banco.php

--- conexao-banco.php
+++ conexao-banco.php
@@ -1,6 +1,14 @@
-<?
-$conexao=pg_connect("host=postg-dev port=5432 dbname=postgres user=postgres password=Pass2020!");
-if(!$conexao){
-echo"Falha na conexão com o banco. Veja detalhes técnicos:".pg_last_error($conexao);
+<?php
+$link = mysqli_connect("HOST", "USUARIO", "SENHA", "BASE");
+ 
+if (!$link) {
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
-?>
+ 
+echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+ 
+mysqli_close($link);
+?> 
