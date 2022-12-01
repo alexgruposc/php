c546e7a83f3625bdc2f6be048beef7b32da3a76d
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2022-12-01 20:38:43 -0300

    Update conexaomy.php

--- conexaomy.php
+++ conexaomy.php
@@ -5,12 +5,13 @@ $user = getenv("databaseuser");
 $pass = getenv("databasepassword");
 $db =   getenv("databasename");
 
-$link=mysqli_connect($host, $user, $pass, $db);
+$con=mysqli_connect($host, $user, $pass, $db);
 
-if(!$link) 
+if(!$con) 
 {
         echo("Sem sucesso");
  }
+
 else
 {
 echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
