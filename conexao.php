1fb6815ffa1bd1343d2660440aa0d8a6dbb62ec5
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2022-12-01 18:33:30 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -13,10 +13,16 @@ if(isset($_POST["acao"])){
     }
 }
 
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("databaseuser");
+$pass = getenv("databasepassword");
+$db =   getenv("databasename");
 
 // Responsável por criar uma conexão com meu banco
 function abrirBanco() {
-    $conexao = new mysqli("172.17.0.9", "userIYE", "BNuywMs74q1kEGav", "sampledb");
+    
+    $conexao = new mysqli($host, $user, $pass, "sampledb");
     return $conexao;
 }
 
