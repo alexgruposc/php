7022fbd430a54feaf6d776bcdd997576f4392eae
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2022-12-01 20:15:22 -0300

    Update conexaomy.php

--- conexaomy.php
+++ conexaomy.php
@@ -7,7 +7,8 @@ $link = mysqli_connect($host, $user, $pass, $db);
 
 echo $link";
 
-if (!$link) {
+if(!$link) 
+{
     echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
