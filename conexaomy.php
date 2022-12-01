c83581e97353ccd9e2666acf255c65fd00867a3d
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2022-12-01 20:32:42 -0300

    Update conexaomy.php

--- conexaomy.php
+++ conexaomy.php
@@ -1,15 +1,15 @@
 <?php
+
 $host = getenv("MYSQL_SERVICE_HOST");
 $user = getenv("databaseuser");
 $pass = getenv("databasepassword");
 $db =   getenv("databasename");
+
 $link=mysqli_connect($host, $user, $pass, $db);
 
 if(!$link) 
 {
-
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        echo ("Sem sucesso");
  }
 
 else
