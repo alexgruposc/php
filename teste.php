84eb8f8f196297c31356310700b0e0b10249b8b0
alexgruposc <88335352+alexgruposc@users.noreply.github.com>
2022-12-06 17:40:11 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,3 +1,11 @@
 <?php
+$host = getenv("MYSQL_SERVICE_HOST");
 
+$user = getenv("databaseuser");
+$pass = getenv("databasepassword");
+$db =   getenv("databasename");
+    
+    $conexao = new mysqli($host, $user, $pass, $db);
+    echo $host;
+    return $conexao;
 ?>
